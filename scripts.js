18f73549b80395ae84faa5df31fb3496dc087633
Fernando Neves <guzzoneves_fernando@outlook.com>
2022-05-04 18:18:20 -0300

    corrige curso saindo do input de quantidade

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 {
     const giftSidebarEl = document.querySelector('.gift-sidebar')
-    function openSidebar () {   
+    function openSidebar() {
         giftSidebarEl.classList.add('gift-sidebar-open')
     }
-    function closeSidebar () {
+    function closeSidebar() {
         giftSidebarEl.classList.remove('gift-sidebar-open')
     }
     const btnGiftEl = document.getElementById('btn-gift')
     btnGiftEl.addEventListener('click', openSidebar)
-    
+
     const btnCloseGiftEl = document.querySelector('#btn-close-gift')
-    btnCloseGiftEl.addEventListener('click', closeSidebar) 
-    }
-    
-    const fetchProducts = () => {
-        const groupsRootEl = document.querySelector('#groups-root')
-       fetch('/productslist.json') 
+    btnCloseGiftEl.addEventListener('click', closeSidebar)
+}
+
+const fetchProducts = () => {
+    const groupsRootEl = document.querySelector('#groups-root')
+    fetch('/productslist.json')
         .then(res => res.json())
         .then(data => {
             groupsRootEl.innerHTML = ''
@@ -27,20 +27,20 @@
         .catch(() => {
             groupsRootEl.innerHTML = '<p class="error-alert">Falha ao buscar produtos. <br>Por favor, tente novamente.</p>'
         })
-    }
-    const getSectionElement = (group) => {
-        const sectionEl = document.createElement('section')
-        const sectionTitleEl = document.createElement('h2')
-        sectionTitleEl.textContent = group.name
-        sectionEl.appendChild(sectionTitleEl)
-        const productsGridEl =  document.createElement('div')
-        productsGridEl.classList.add('products-grid')
-        sectionEl.appendChild(productsGridEl)
-        group.products.forEach((product) => {
-            const cardWrapEl = document.createElement('article')
-            cardWrapEl.classList.add('card')
-            cardWrapEl.innerHTML = 
-                `<img src="${product.image}" width="316" height="193" alt="
+}
+const getSectionElement = (group) => {
+    const sectionEl = document.createElement('section')
+    const sectionTitleEl = document.createElement('h2')
+    sectionTitleEl.textContent = group.name
+    sectionEl.appendChild(sectionTitleEl)
+    const productsGridEl = document.createElement('div')
+    productsGridEl.classList.add('products-grid')
+    sectionEl.appendChild(productsGridEl)
+    group.products.forEach((product) => {
+        const cardWrapEl = document.createElement('article')
+        cardWrapEl.classList.add('card')
+        cardWrapEl.innerHTML =
+            `<img src="${product.image}" width="316" height="193" alt="
                            ${product.name}"/><div class="card-content">
                            ${product.name ? `<h4>
                            ${product.name}</h4>` : ''}
@@ -50,89 +50,94 @@
                            ${product.points} pontos</h5>` : ''}
                 <button class="btn">Resgatar</button>
             </div>`
-    
-            const btnAddGiftEl = cardWrapEl.querySelector('button')
-            btnAddGiftEl.addEventListener('click', () => {
-                addToCart(product)
-            })
-            productsGridEl.appendChild(cardWrapEl)
+
+        const btnAddGiftEl = cardWrapEl.querySelector('button')
+        btnAddGiftEl.addEventListener('click', () => {
+            addToCart(product)
         })
-    
-        return sectionEl
-    }
-    fetchProducts()
-    
-    let productsCart = []
-    const addToCart = newProduct => {
-        const productIndex = productsCart.findIndex(
-            item => item.id === newProduct.id
-        )
-        if (productIndex === -1) {
-            productsCart.push({
+        productsGridEl.appendChild(cardWrapEl)
+    })
+
+    return sectionEl
+}
+fetchProducts()
+
+let productsCart = []
+const addToCart = newProduct => {
+    const productIndex = productsCart.findIndex(
+        item => item.id === newProduct.id
+    )
+    if (productIndex === -1) {
+        productsCart.push({
             ...newProduct,
             qty: 1
-            })
-        } else {
-            productsCart[productIndex].qty++
-        }
-    
-        handleCartUpdate()
-    }
-    const removeOfGift = id => {
-        productsCart = productsCart.filter((product) => {
-            if (product.id === id) {
-                return false
-            }
-            return true
         })
-        handleCartUpdate()
+    } else {
+        productsCart[productIndex].qty++
     }
-    const updateItemQty = (id, newQty) => {
-        productsCart.findIndex((product) => {
-            if (product.id === id) {
-                return true
-            }
+    handleCartUpdate()
+}
+const removeOfGift = id => {
+    productsCart = productsCart.filter((product) => {
+        if (product.id === id) {
             return false
-        })
-    handleCartUpdate() 
+        }
+        return true
+    })
+    handleCartUpdate()
+}
+const updateItemQty = (id, newQty) => {
+    if (parseInt(newQty) > 5) {
+        alert('Quantidade extrapola o limite de 5.')
     }
-    const handleCartUpdate = () => {
-        const emptyCardEl = document.querySelector('#empty-gift')
-        const cardWithProductsEl = document.querySelector('#card-with-products')
-        const cardProductsListEl = cardWithProductsEl.querySelector('ul')
-        const btnCartBadgeEl = document.querySelector('.btn-cart-badge')
-        if (productsCart.length > 0) {
-            //ATUALIZA A BADGE
-            btnCartBadgeEl.classList.add('btn-cart-badge-show')
-            let total = 0
-            let totalPointsGift = 0
-            productsCart.forEach((product) => {
-                total = total + product.qty
-                totalPointsGift = totalPointsGift + product.points * product.qty
-            })
-            //ATUALIZA A BADGE
-            const cardTotalEl = document.querySelector('.card-total p:last-child')
-            cardTotalEl.textContent = totalPointsGift
-            btnCartBadgeEl.textContent = total
-            //EXIBE CARRINHO
-            cardWithProductsEl.classList.add('card-with-products-show')
-            //ATUALIZA O TOTAL DO CARRINHO
-            emptyCardEl.classList.remove('empty-gift-show')
-            //EXIBIR ARRAY DE PRODUTOS CRIADOS
+    const productIndex = productsCart.findIndex((product) => {
+        if (product.id === id) {
+            return true
+        }
+        return false
+    })
+    productsCart[productIndex].qty = parseInt(newQty)
+    
+    handleCartUpdate(false)
+}
+const handleCartUpdate = (renderItens = true) => {
+    const emptyCardEl = document.querySelector('#empty-gift')
+    const cardWithProductsEl = document.querySelector('#card-with-products')
+    const cardProductsListEl = cardWithProductsEl.querySelector('ul')
+    const btnCartBadgeEl = document.querySelector('.btn-cart-badge')
+    if (productsCart.length > 0) {
+        //ATUALIZA A BADGE
+        btnCartBadgeEl.classList.add('btn-cart-badge-show')
+        let total = 0
+        let totalPointsGift = 0
+        productsCart.forEach((product) => {
+            total = total + product.qty
+            totalPointsGift = totalPointsGift + product.points * product.qty
+        })
+        //ATUALIZA A BADGE
+        const cardTotalEl = document.querySelector('.card-total p:last-child')
+        cardTotalEl.textContent = totalPointsGift
+        btnCartBadgeEl.textContent = total
+        //EXIBE CARRINHO
+        cardWithProductsEl.classList.add('card-with-products-show')
+        //ATUALIZA O TOTAL DO CARRINHO
+        emptyCardEl.classList.remove('empty-gift-show')
+        //EXIBIR ARRAY DE PRODUTOS CRIADOS
+        if (renderItens) {
             cardProductsListEl.innerHTML = ''
             productsCart.forEach((product) => {
                 const listItemEl = document.createElement('li')
                 listItemEl.innerHTML = `
-                <img src="${product.image}" alt="
-                          ${product.name}" width="70" height="70" /><div><p class="h4">
-                          ${product.name}</p><p class="points">
-                          ${product.points}</p>
-                </div>
-                <input class="form-input" type="number" value="${product.qty}" />
-                <button>
-                    <i class="fa-solid fa-trash-can"></i>
-                </button>
-                `
+                    <img src="${product.image}" alt="
+                            ${product.name}" width="70" height="70" /><div><p class="h4">
+                            ${product.name}</p><p class="points">
+                            ${product.points}</p>
+                    </div>
+                    <input class="form-input" type="number" value="${product.qty}" />
+                    <button>
+                        <i class="fa-solid fa-trash-can"></i>
+                    </button>
+                    `
                 const btnRemoveEl = listItemEl.querySelector('button')
                 btnRemoveEl.addEventListener('click', () => {
                     removeOfGift(product.id)
@@ -140,11 +145,21 @@
                 const inputQtyEL = listItemEl.querySelector('input')
                 inputQtyEL.addEventListener('keyup', (event) => {
                     updateItemQty(product.id, event.target.value)
-                    console.log('apertou', event.target.value)
+                })
+                inputQtyEL.addEventListener('keydown', (event) => {
+                    console.log('executou keydown', event.target.value)
+                    if (event.key === '-' || event.key === '.' || event.key === ',') {
+                        event.preventDefault()
+                    }
+                })
+                
+                inputQtyEL.addEventListener('change', (event) => {
+                    updateItemQty(product.id, event.target.value)
                 })
                 cardProductsListEl.appendChild(listItemEl)
             })
-    } else {
+        }
+        } else {
         //ESCONDER BADGE
         btnCartBadgeEl.classList.remove('btn-cart-badge-show')
         //EXIBIR CARRINHO VAZIO
